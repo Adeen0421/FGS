b39247ac74d72a47a71e5d98bcae1d7715bad250
Adeen0421 <159934110+Adeen0421@users.noreply.github.com>
2025-06-17 17:03:30 +0500

    S

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,16 +1,8 @@
 'use client'
 
-import { NextStudio } from 'next-sanity/studio'
-import { createClient } from 'next-sanity'
+import { Studio } from 'sanity'
 import config from '../../../../sanity.config'
 
-const client = createClient({
-  projectId: config.projectId,
-  dataset: config.dataset,
-  apiVersion: '2024-03-13',
-  useCdn: false,
-})
-
 export default function StudioPage() {
-  return <NextStudio client={client} />
+  return <Studio config={config} />
 } 
